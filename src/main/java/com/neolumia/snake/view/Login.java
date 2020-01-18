2185372c73ce405a21f36a399d35faa3528c1cb1
fadi <frouhana@outlook.com>
2020-01-18 10:06:53 +0200

    documenting osama section

--- src/main/java/com/neolumia/snake/view/Login.java
+++ src/main/java/com/neolumia/snake/view/Login.java
@@ -32,41 +32,21 @@ import java.sql.SQLException;
 /** this class controls the login windows that is shown in order to manage users */
 public class Login extends Window {
   GameApp app;
-  @FXML
-  JFXTextField playerName;
-  @FXML
-  JFXPasswordField password;
+  @FXML JFXTextField playerName;
+  @FXML JFXPasswordField password;
   @FXML Text password_text;
   @FXML AnchorPane ap;
-  @FXML
-  JFXButton login;
+  @FXML JFXButton login;
   @FXML JFXButton next;
-  Boolean isNew = false;
-  Boolean isValidPassword = false;
-  @FXML
-  private JFXButton loginSignup;
-
-  @FXML
-  private JFXButton Back;
-
-  @FXML
-  private JFXTextField NewPlayerName;
-
-  @FXML
-  private Text password_text1;
-
-  @FXML
-  private JFXPasswordField NewPassword;
-
-  @FXML
-  private JFXPasswordField NewPassword1;
-
-  @FXML
-  private JFXButton signup;
-  @FXML
-  private Group grpSignup;
-  @FXML
-  private Group grpLogin;
+  @FXML private JFXButton loginSignup;
+  @FXML private JFXButton Back;
+  @FXML private JFXTextField NewPlayerName;
+  @FXML private Text password_text1;
+  @FXML private JFXPasswordField NewPassword;
+  @FXML private JFXPasswordField NewPassword1;
+  @FXML private JFXButton signup;
+  @FXML private Group grpSignup;
+  @FXML private Group grpLogin;
   RequiredFieldValidator reqUserValidator = new RequiredFieldValidator();
   RequiredFieldValidator reqPasswordValidator = new RequiredFieldValidator();
 
@@ -77,10 +57,8 @@ public class Login extends Window {
    */
   public Login(GameApp app) {
     this.app = app;
-
     password.setVisible(false);
     login.setVisible(false);
-
     playerName
         .textProperty()
         .addListener(
@@ -90,7 +68,6 @@ public class Login extends Window {
 
     reqUserValidator.setMessage("User Name is Required");
     reqPasswordValidator.setMessage("Password is Required");
-
     playerName.getValidators().add(reqUserValidator);
     password.getValidators().add(reqPasswordValidator);
     NewPlayerName.getValidators().add(reqUserValidator);
@@ -107,53 +84,19 @@ public class Login extends Window {
    */
   @FXML
   public void login(MouseEvent mouseEvent) throws SQLException {
-
-
-    if(playerName.validate()){
-      if(password.validate()){
-        if(app.getDatabase().playerPassword(playerName.getText()).equals(password.getText())){
+    if (playerName.validate()) {
+      if (password.validate()) {
+        if (app.getDatabase().playerPassword(playerName.getText()).equals(password.getText())) {
           Toast.toast("Logged In Successfully " + playerName.getText(), Color.GREEN);
           String playerName = this.playerName.getText();
           app.setPlayerName(playerName);
           app.getWindowManager().request(new MenuWindow(app));
 
-        }else{
+        } else {
           Toast.toast("Invalid Password", Color.RED);
-
         }
-
-
-
       }
     }
-
-
-
-    /*
-    if (isNew) {
-      if (password.getText().length() < 6 || password.getText().length() > 12)
-        Toast.toast("Invalid Password Length, Length must be 6-12 chars", Color.RED);
-      else {
-        Toast.toast("Registered Successfully " + playerName.getText(), Color.GREEN);
-        isValidPassword = true;
-      }
-
-    } else {
-      if (password.getText().equals("a")) // TODO : Check PW from DB
-      {
-        Toast.toast("Logged In Successfully " + playerName.getText(), Color.GREEN);
-        isValidPassword = true;
-      } else {
-        Toast.toast("Invalid Password ! Please Try Again", Color.RED);
-        isValidPassword = false;
-      }
-    }
-
-    if (isValidPassword) {
-      String playerName = this.playerName.getText();
-      app.setPlayerName(playerName);
-      app.getWindowManager().request(new MenuWindow(app));
-    }*/
   }
 
   /**
@@ -167,7 +110,6 @@ public class Login extends Window {
   public void next(MouseEvent mouseEvent) throws SQLException {
     String name = "";
 
-
     if (playerName.validate()) {
       name = this.playerName.getText();
       if (app.getDatabase().player_ID(playerName.getText()) != -1) {
@@ -176,15 +118,17 @@ public class Login extends Window {
         next.setDisable(true);
         password.setVisible(true);
         login.setVisible(true);
-      } else {//username does not exist
+      } else { // username does not exist
         Toast.toast("User does Not Exist", Color.RED);
-
       }
     }
   }
 
-
-
+  /**
+   * backToLogin - returns the user to login page and hides signup
+   *
+   * @param event
+   */
   @FXML
   void backToLogin(MouseEvent event) {
     grpSignup.setVisible(false);
@@ -195,56 +139,50 @@ public class Login extends Window {
     NewPassword1.clear();
   }
 
-
+  /**
+   * navSignup - enable user to register/create a new account
+   *
+   * @param event
+   */
   @FXML
   void navSignup(MouseEvent event) {
     grpLogin.setVisible(false);
     grpSignup.setVisible(true);
     playerName.clear();
     password.clear();
-
-
   }
 
-
+  /**
+   * signupNewPlayer - handles adding a new player - clicking submit on signup window
+   *
+   * @param event
+   * @throws SQLException
+   */
   @FXML
   void signupNewPlayer(MouseEvent event) throws SQLException {
-      if(NewPlayerName.validate()) {
-        String playername = NewPlayerName.getText();
-        if (app.getDatabase().player_ID(playername) != -1) {
-          //user already Exist
-          Toast.toast("User Already Exists ", Color.RED);
+    if (NewPlayerName.validate()) {
+      String playername = NewPlayerName.getText();
+      if (app.getDatabase().player_ID(playername) != -1) {
+        // user already Exist
+        Toast.toast("User Already Exists ", Color.RED);
+      } else { // username do not exist
+        if (NewPassword.validate()) {
+          if (NewPassword1.validate()) {
+            if (NewPassword.getText().equals(NewPassword1.getText())) {
+              app.getDatabase().newPlayer(playername, NewPassword.getText());
+              grpSignup.setVisible(false);
+              grpLogin.setVisible(true);
+              playerName.setText(NewPlayerName.getText());
+              NewPlayerName.clear();
+              NewPassword.clear();
+              NewPassword1.clear();
 
-
-        } else {// username do not exist
-          if (NewPassword.validate()) {
-            if (NewPassword1.validate()) {
-              if (NewPassword.getText().equals(NewPassword1.getText())) {
-                app.getDatabase().newPlayer(playername, NewPassword.getText());
-                grpSignup.setVisible(false);
-                grpLogin.setVisible(true);
-                playerName.setText(NewPlayerName.getText());
-                NewPlayerName.clear();
-                NewPassword.clear();
-                NewPassword1.clear();
-
-              } else { //passwords do not match
-                Toast.toast("Passwords Do Not Match !", Color.RED);
-
-              }
-
-
+            } else { // passwords do not match
+              Toast.toast("Passwords Do Not Match !", Color.RED);
             }
           }
-
-
         }
-
-
-      }}
-
-
-
-
-
+      }
+    }
+  }
 }
